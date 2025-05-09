0349321a39a00e6704ca544a58d62e092dbebc57
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2025-05-09 14:50:38 +0300

    code refactor

--- src/components/Loader/loader.jsx
+++ src/components/Loader/loader.jsx
@@ -1,14 +1,9 @@
-import React from 'react';
-import './loader.css';
+import React from "react";
+import "./loader.css";
 
 // Loader component to display a loading spinner
 const Loader = () => {
-    return (
-        <div className="loader">
-            {/* Spinner element for the loading animation */}
-            <div className="spinner"></div>
-        </div>
-    );
+  return <div className="loader"></div>;
 };
 
 export default Loader;
